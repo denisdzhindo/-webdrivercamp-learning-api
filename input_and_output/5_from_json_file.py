946198c1afefdb19af183dfe569188210dee7d0f
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-11-15 21:09:27 -0800

    adding api

---
+++ input_and_output/5_from_json_file.py
@@ -0,0 +1,33 @@
+import json
+
+def from_json_file(file_name):
+    with open(file_name) as file:
+        data = json.load(file)
+    return(data)
+
+if __name__=="__main__":
+    import os
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_name = f"{dir_path}/data.json"
+    
+    data_object = from_json_file(file_name)
+
+    print(data_object)
+    print(type(data_object))
+    print()
+    for key, val in data_object.items():
+        print(f"{key}: {type(val).__name__}")
+                    
+
+
+
+
+
+
+
+    
+                    
+
+
+
