f57036b24a71e0498999e0c8b44624dc640d4e86
Denis Dzhindo <santacruz@Deniss-MacBook-Pro.local>
2024-11-17 12:18:11 -0800

    adding requests

--- requests/0_get_all_repos.py
+++ requests/0_get_all_repos.py
@@ -4,27 +4,26 @@ def get_repos(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        data = response.json()  # Parse the JSON response
-        count = data.get('total_count', 0)  # Get the total count of repositories
-        list_ = data.get('items', [])  # Get the list of repositories under 'items'
-
-        list_of_items_sorted = sorted(list_, key=lambda item: item['full_name'].lower())
-
-        return list_of_items_sorted, count, response.status_code
+        data = response.json()
+        list_ = data.get('items', [])  # Get the list of repositories under 'items', if there is no 'items' -> []
+        list_of_items_sorted = sorted(list_, key=lambda x: x['full_name'].lower())
+        return list_of_items_sorted, response.status_code
     else:
         print(f"Error: {response.status_code}")
         return [], 0, response.status_code  # Return empty list and zero count in case of an error
 
-
 if __name__ == "__main__":
 
     wlp_url = "https://api.github.com/search/repositories?q=webdrivercamp-learning-python"
-    list_of_items, total_count, status_code = get_repos(wlp_url)
+    list_of_items, status_code = get_repos(wlp_url)
+
+    print(f"\nResponse status code: {status_code}")
+    print(f"Total count of found items: {len(list_of_items)}")
+    print()
 
     for element in list_of_items:
         user = element['owner']['login']  # Get the owner username
         repo = element['name']  # Get the repository name
-        print(f"{user:15}", repo)
+        print(f"{user:20}", repo)
 
-    print(f"\nResponse status code: {status_code}")
-    print(f"Total count of found items: {total_count}")
+
--- requests/test_reqests.py
+++ requests/test_reqests.py
@@ -1,16 +1,54 @@
 import requests
 
-r = requests.get('https://xkcd.com/353/')
-print(r)
+#r = requests.get('https://xkcd.com/353/') # ger requests
+# print(r)
 
 # print(dir(r))
 # print(help(r)) # everything of this object
-# print(r.text) # html of the website
+# print(r.text) # HTML of the website
 
-r = requests.get(' https://imgs.xkcd.com/comics/python.png') # png image from that website
+# r = requests.get(' https://imgs.xkcd.com/comics/python.png') # png image from that website
 # print(r.content) # content of that png image
 
-with open('comic.png', 'wb') as f:
-    f.write(r.content)
+# with open('comic.png', 'wb') as f:  # downloaded the website png picture
+    # f.write(r.content)
 
+# print(r.status_code)
+# print(r.ok) # this return True for anything below 400 responds, so if client/server error - returns False
+# print(r.headers)
+
+''' 
+# get
+payload = {'page': 2, 'count': 25} # add parameters to url
+r = requests.get('https://httpbin.org/get', params=payload)
+
+print(r.text) # print got requests
+print(r.url)
+'''
+
+'''
+# post
+payload = {'username': 'denis', 'password': 'testing'}
+r = requests.post('https://httpbin.org/post', data=payload)
+# print(r.text) # prints the request
+# print(r.json()) # prints JSON dict
+r_dict = r.json()
+print(r_dict)
+print(r_dict['form'])
+'''
+
+# http://httpbin.org/#/HTTP_Methods get Auth/Get url /basic-auth/{user}/{passwd}
+# Prompts the user for authorization using HTTP Basic Auth
+# https://httpbin.org/basic-auth/denis/testing - run this url and get auth name/pass
+
+'''
+r = requests.get('https://httpbin.org/basic-auth/denis/testing', auth=('denis', 'testing'))
+print(r.text)
+
+r = requests.get('https://httpbin.org/basic-auth/denis/testing', auth=('deniss', 'testing'))
+print(r) # prints 401 error - unauthorized user
+'''
+
+# r = requests.get('https://httpbin.org/delay/6', timeout=3) # make timeout if a page is not loading
+# print(r)
 
